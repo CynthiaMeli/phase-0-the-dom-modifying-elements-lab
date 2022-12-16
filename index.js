adea481a2c3cf0e65688c60e0a80f973618faf3b
Cynthia Meli <cynthiagwyneth243@gmail.com>
2022-12-16 16:22:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+var body = document.querySelector('body');
+body.appendChild(newHeader);
+document.getElementsByTagName("h1")[0].innerHTML="CYNTHIA-MELI is the champion";
